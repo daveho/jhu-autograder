eb3db66b10ec73133d921bc9a118e4de67d101cf
David Hovemeyer <david.hovemeyer@gmail.com>
2019-10-31 12:28:44 -0400

    add TestGroup, preparation steps, MUnitTest class
    This is starting to be vaguely useful.

--- autograder.rb
+++ autograder.rb
@@ -1,15 +1,17 @@
 # Framework for creating Gradescope autograders
 
+require 'open3'
+
 ########################################################################
-# Main object model classes.
+# Main object model classes
 ########################################################################
 
 # Rubric: collection of RubricItems
 class Rubric
   attr_reader :items
 
-  def initialize
-    @items = []
+  def initialize(items: [])
+    @items = items
   end
 
   def add(item)
@@ -87,6 +89,10 @@ class TestResult
     @score = b ? @rubric_item.max_score : 0.0
   end
 
+  def fail
+    @score = 0.0
+  end
+
   def to_json_obj
     return {
       'name' => @rubric_item.name,
@@ -95,6 +101,47 @@ class TestResult
       'output' => @diagnostics.join("\n"),
       'visibility' => @test.is_visible
     }
+  end
+end
+
+class AutofailTest < Test
+  def initialize(test, fail_msg)
+    super(test.rubric_item_id)
+    @fail_msg = fail_msg
+  end
+
+  def execute(autograder, test_result)
+    test_result.add_diagnostic(@fail_msg)
+    test_result.fail
+  end
+end
+
+class TestGroup
+  attr_reader :members
+
+  def initialize(prep: [], tests: [])
+    @prep = prep
+    @members = tests
+  end
+
+  def prepare
+    prep_msgs = []
+    begin
+      @prep.each do |prep|
+        #puts "Executing preparation step #{prep.class}"
+        prep.execute(prep_msgs)
+      end
+      # All preparation steps succeded.
+      # Could do something with prep_msgs, although
+      # they're not really part of a specific test.
+    rescue => ex
+      # Make all tests auto-fail
+      wrapped_tests = []
+      @members.each do |test|
+        wrapped_tests.push(AutofailTest.new(test, prep_msgs.last))
+      end
+      @members = wrapped_tests
+    end
   end
 end
 
@@ -110,11 +157,25 @@ class Autograder
   end
 
   def execute
+    # if the /autograder directory exists, chdir to it,
+    # otherwise just stay where we are (e.g., for local testing)
+    if File.directory?('/autograder')
+      Dir.chdir('/autograder')
+    end
+
     @tests.each do |test|
-      rubric_item = @rubric.lookup_item(test.rubric_item_id)
-      test_result = TestResult.new(test, rubric_item)
-      test.execute(self, test_result)
-      @test_results.push(test_result)
+      if test.is_a?(TestGroup)
+        # Test group: execute preparation step(s), then execute
+        # all tests.  Note that tests will auto-fail themselves if
+        # any preparation step(s) failed.
+        test.prepare
+        test.members.each do |test|
+          execute_test(test)
+        end
+      else
+        # Just executing one standalone test
+        execute_test(test)
+      end
     end
 
     report = { 'tests' => [] }
@@ -124,14 +185,19 @@ class Autograder
     return report
   end
 
-  # Execute a block in the submission directory
-  def in_submission_dir
-    begin
-      Dir.chdir('submission')
-      yield
-    ensure
-      Dir.chdir('..')
-    end
+  def execute_test(test)
+    rubric_item = @rubric.lookup_item(test.rubric_item_id)
+    test_result = TestResult.new(test, rubric_item)
+    test.execute(self, test_result)
+    @test_results.push(test_result)
+  end
+end
+
+# Factory to create preparation steps for a TestGroup
+class Prep
+  # Run specific command, allowing redirection to/from files
+  def self.run_command(cmd: nil, input: nil, output: nil)
+    return RunCommand.new(cmd, input, output)
   end
 end
 
@@ -139,6 +205,7 @@ end
 # Test implementation classes
 ########################################################################
 
+# Check that required files are present in the submission directory.
 class TestCheckFiles < Test
   def initialize(rubric_item_id, file_list)
     super(rubric_item_id)
@@ -147,13 +214,71 @@ class TestCheckFiles < Test
 
   def execute(autograder, test_result)
     count = 0
-    autograder.in_submission_dir do
-      @file_list.each do |filename|
-        exists = File.file?(filename)
-        test_result.add_diagnostic("#{filename} exists: #{exists}")
-        count += 1 if exists
-      end
+    @file_list.each do |filename|
+      exists = File.file?("submission/#{filename}")
+      test_result.add_diagnostic("#{filename} exists: #{exists}")
+      count += 1 if exists
     end
     test_result.pass_if(count == @file_list.length)
+  end
+end
+
+# MUnit test (https://cis.gvsu.edu/~kurmasz/Software/mipsunit_munit/).
+# munit.jar must be in the autograder root directory (/autograder).
+class MUnitTest < Test
+  def initialize(rubric_item_id, asm_filename, testclass_filename, show_munit_output: false)
+    super(rubric_item_id)
+    @asm_filename = asm_filename
+    @testclass_filename = testclass_filename
+    @show_munit_output = show_munit_output
+  end
+
+  def execute(autograder, test_result)
+    cmd = ['java', '-jar', 'munit.jar', @asm_filename, @testclass_filename]
+    stdout_str, stderr_str, status = Open3.capture3(*cmd, stdin_data: '')
+    test_result.pass_if(status.success?)
+    if status.success?
+      test_result.add_diagnostic("MUnit test #{@testclass_filename} passed")
+    else
+      test_result.add_diagnostic("MUnit test #{@testclass_filename} failed")
+      if @show_munit_output
+        test_result.add_diagnostic('')
+        stdout_str.split('\n').each do |line|
+          test_result.add_diagnostic(line)
+        end
+      end
+    end
+  end
+end
+
+########################################################################
+# Preparation steps initialization
+########################################################################
+
+class RunCommand
+  def initialize(cmd, input, output)
+    @cmd = cmd
+    @input = input
+    @output = output
+  end
+
+  def execute(prep_msgs)
+    #puts "run_command: cmd=#{@cmd}, input=#{@input}, output=#{@output}"
+    prep_msgs.push("Running command #{@cmd}")
+    raise "run_script missing command" if @cmd.nil?
+    @input = '/dev/null' if @input.nil?
+    input_data = File.read(@input)
+    stdout_str, stderr_str, status = Open3.capture3(*@cmd, stdin_data: input_data)
+    #puts "stdout_str=#{stdout_str}"
+    if !status.success?
+      prep_msgs.push("Command #{cmd} failed")
+      raise "#{cmd[0]} command failed"
+    end
+    # If output was redirected to file, write it
+    if !@output.nil?
+      File.open(@output, 'w') do |outf|
+        outf.write(stdout_str)
+      end
+    end
   end
 end
